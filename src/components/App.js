cbc9e47fe964785fe5e97ea57d0cb100f6ca0004
Dmytro <tschernomord@gmail.com>
2021-01-07 09:58:42 +0100

    add Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import SearchForm from "./SearchForm/searchForm";
+import Header from "./Header/Header";
+import UserInfo from "./UserInfo/UserInfo";
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <div className="App">
-        <SearchForm />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Header>
+            <UserInfo />
+          </Header>
+          <SearchForm />
+        </div>
+      </BrowserRouter>
     </>
   );
 }
