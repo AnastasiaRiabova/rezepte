f6f163282b1976843883319ba8f0677881b2e7f8
AnastasiaRiabova <AnastasiaRiabova@users.noreply.github.com>
2021-01-08 10:56:34 +0100

    add persistore

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
-import logo from '../../images/logo.svg'
-import styles from './Header.module.css'
+import logo from '../../images/logo.svg';
+import styles from './Header.module.css';
 export default function Header({ children }) {
-    console.log(children);
-    return (
-        <header className={styles.header}>
-            <img src={logo} alt="logo" />
-            {children}
-        </header>
-    )
+  // console.log(children);
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt="logo" />
+      {children}
+    </header>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// import persistor from './Redux/store';
 import App from './components/App';
-import store from './Redux/store';
+import store, { persistor } from './Redux/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
