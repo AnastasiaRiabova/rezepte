f6ecddcd4889354266c181a06a8877d59f798093
Dmytro <tschernomord@gmail.com>
2021-01-08 09:02:57 +0100

    add dropbox

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import logo from '../../images/logo.svg'
 import styles from './Header.module.css'
 export default function Header({ children }) {
-    console.log(children);
     return (
         <header className={styles.header}>
             <img src={logo} alt="logo" />
--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import { useState } from 'react'
 import cross from '../../images/cross.svg'
 import burger from '../../images/burger.svg'
+import DropBox from "../DropBox/DropBox";
 
 export default function UserInfo() {
     const [token, setToken] = useState(false)
@@ -12,7 +13,7 @@ export default function UserInfo() {
             {token ? "Name" : null}
             {token ? <NavLink onClick={() => setToken(!token)} to="/logout">Logout</NavLink> : <NavLink onClick={() => setToken(!token)} to="/login">Login</NavLink>}
             {menu ? <img onClick={() => setMenu(!menu)} src={cross} alt="cross" /> : <img onClick={() => setMenu(!menu)} src={burger} alt="burger" />}
-            {menu ? <div>меню</div> : null}
+            {menu ? <DropBox /> : null}
         </div>
     )
 }
