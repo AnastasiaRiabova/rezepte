72125bcc3021ba63402a36546c65148d41057f5f
Dmytro <tschernomord@gmail.com>
2021-01-13 11:35:55 +0100

    done Validation withoit styles

--- src/components/View/LoginView/LoginView.js
+++ src/components/View/LoginView/LoginView.js
@@ -4,44 +4,43 @@ import Button from '../../UIComponents/Button/Button';
 import CustomNavlink from '../../UIComponents/NavLink/CustomNavlink';
 import Input from '../../UIComponents/Input/Input';
 import operations from '../../../Redux/Auth/auth-operations';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import styles from "./LoginView.module.css";
 
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+});
 export default function LoginView() {
-  const [email, setEmail] = useState('');
-  const handleNameChange = e => setEmail(e.currentTarget.value);
-  const [password, setPassword] = useState('');
-  const handlePasswordChange = e => setPassword(e.currentTarget.value);
 
   const dispatch = useDispatch();
-  const onLogin = useCallback(() => {
-    dispatch(operations.logIn({ email, password }));
-  }, [dispatch, email, password]);
+  const onLogin = useCallback((data) => {
+    dispatch(operations.logIn(data));
+  }, [dispatch]);
 
-  const handleOnsubmit = e => {
-    e.preventDefault();
-    onLogin();
-    setPassword('');
-    setEmail('');
-  };
   return (
     <div>
       LOGIN
-      <form onSubmit={handleOnsubmit}>
-        <Input
-          onChange={handleNameChange}
-          placeholder="name"
-          type="text"
-          value={email}
-        />
-        <Input
-          onChange={handlePasswordChange}
-          placeholder="password"
-          type="password"
-          value={password}
-        />
-        {/* <button onClick={handleOnsubmit} type='submit'>Login</button> */}
-        <Button color="white" label="Login" type="submit" />
-      </form>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validationSchema={LoginSchema}
+        onSubmit={async (values, { resetForm }) => {
+          await onLogin(values);
+          resetForm()
+        }}
+      >
+        {(({ errors, touched }) => <Form>
+          <Field placeholder='Email' className={styles.input} name='email' />{errors.email && touched.email ? (<div>{errors.email}</div>) : null}
+          <Field type='password' placeholder='Password' className={styles.input} name='password' />{errors.password && touched.password ? (<div>{errors.password}</div>) : null}
+          <Button label="Login" color="orange" type="submit" />
+        </Form>)}
+      </Formik>
       <CustomNavlink to="/registration" label="Registration" color="orange" />
     </div>
   );
 }
+
--- src/components/View/RegistrationView/RegistrationView.js
+++ src/components/View/RegistrationView/RegistrationView.js
@@ -4,85 +4,44 @@ import Button from '../../UIComponents/Button/Button';
 import Input from '../../UIComponents/Input/Input';
 import operation from '../../../Redux/Auth/auth-operations';
 import CustomNavlink from '../../UIComponents/NavLink/CustomNavlink';
-import { ValidationSchemaExample } from '../../Formik';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import styles from './RegistrationView.module.css'
+
 const SignupSchema = Yup.object().shape({
-  name: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+  username: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
   email: Yup.string().email('Invalid email').required('Required'),
   password: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
 });
 
-
 export default function RegistrationView() {
-  // const [username, setUsername] = useState('');
-  // const handleNameChange = e => setUsername(e.currentTarget.value);
-  // const [email, setEmail] = useState('');
-  // const handleLoginChange = e => setEmail(e.currentTarget.value);
-  // const [password, setPassword] = useState('');
-  // const handlePasswordChange = e => setPassword(e.currentTarget.value);
-
   const dispatch = useDispatch();
   const onRegistation = useCallback((data) => {
     dispatch(operation.registration(data));
   }, [dispatch]);
-
-  const handleOnsubmit = e => {
-    e.preventDefault();
-    // console.log(username, email, password);
-    onRegistation();
-    // setPassword('');
-    // setUsername('');
-    // setEmail('');
-  };
   return (
     <div>
       REGISTRATION
       <Formik
         initialValues={{
-          name: "",
+          username: "",
           email: '',
           password: '',
         }}
         validationSchema={SignupSchema}
         onSubmit={async (values, { resetForm }) => {
-          // same shape as initial values
           await onRegistation(values)
-          console.log(values);
           resetForm()
         }}
       >
         {(({ errors, touched }) => <Form>
-          <Field placeholder='Name' className={styles.input} name='name' />{errors.name && touched.name ? (<div>{errors.name}</div>) : null}
+          <Field placeholder='Name' className={styles.input} name='username' />{errors.username && touched.username ? (<div>{errors.username}</div>) : null}
           <Field placeholder='Email' className={styles.input} name='email' />{errors.email && touched.email ? (<div>{errors.email}</div>) : null}
-          <Field placeholder='Password' className={styles.input} name='password' />{errors.password && touched.password ? (<div>{errors.password}</div>) : null}
+          <Field type='password' placeholder='Password' className={styles.input} name='password' />{errors.password && touched.password ? (<div>{errors.password}</div>) : null}
           <Button label="Registration" color="orange" type="submit" />
         </Form>)}
       </Formik>
-      {/* <form onSubmit={handleOnsubmit}>
-        <Input
-          type="text"
-          onChange={handleNameChange}
-          placeholder="name"
-          value={username}
-        />
-        <Input
-          onChange={handleLoginChange}
-          placeholder="login"
-          type="text"
-          value={email}
-        />
-        <Input
-          onChange={handlePasswordChange}
-          placeholder="password"
-          type="password"
-          value={password}
-        />
-        <Button label="Registration" color="orange" type="submit" />
-      </form> */}
       <CustomNavlink to="/registration" label='Login' color='orange' />
-      <ValidationSchemaExample />
     </div>
   );
 }
