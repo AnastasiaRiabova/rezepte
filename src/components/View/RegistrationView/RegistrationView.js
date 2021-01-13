f5be477b3fe05d7ad664a0df10e299b3dc955fc6
Dmytro <tschernomord@gmail.com>
2021-01-13 11:13:34 +0100

    add formik

--- src/components/View/RegistrationView/RegistrationView.js
+++ src/components/View/RegistrationView/RegistrationView.js
@@ -4,32 +4,63 @@ import Button from '../../UIComponents/Button/Button';
 import Input from '../../UIComponents/Input/Input';
 import operation from '../../../Redux/Auth/auth-operations';
 import CustomNavlink from '../../UIComponents/NavLink/CustomNavlink';
+import { ValidationSchemaExample } from '../../Formik';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import styles from './RegistrationView.module.css'
+const SignupSchema = Yup.object().shape({
+  name: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+});
+
 
 export default function RegistrationView() {
-  const [username, setUsername] = useState('');
-  const handleNameChange = e => setUsername(e.currentTarget.value);
-  const [email, setEmail] = useState('');
-  const handleLoginChange = e => setEmail(e.currentTarget.value);
-  const [password, setPassword] = useState('');
-  const handlePasswordChange = e => setPassword(e.currentTarget.value);
+  // const [username, setUsername] = useState('');
+  // const handleNameChange = e => setUsername(e.currentTarget.value);
+  // const [email, setEmail] = useState('');
+  // const handleLoginChange = e => setEmail(e.currentTarget.value);
+  // const [password, setPassword] = useState('');
+  // const handlePasswordChange = e => setPassword(e.currentTarget.value);
 
   const dispatch = useDispatch();
-  const onRegistation = useCallback(() => {
-    dispatch(operation.registration({ username, email, password }));
-  }, [dispatch, username, email, password]);
+  const onRegistation = useCallback((data) => {
+    dispatch(operation.registration(data));
+  }, [dispatch]);
 
   const handleOnsubmit = e => {
     e.preventDefault();
     // console.log(username, email, password);
     onRegistation();
-    setPassword('');
-    setUsername('');
-    setEmail('');
+    // setPassword('');
+    // setUsername('');
+    // setEmail('');
   };
   return (
     <div>
       REGISTRATION
-      <form onSubmit={handleOnsubmit}>
+      <Formik
+        initialValues={{
+          name: "",
+          email: '',
+          password: '',
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={async (values, { resetForm }) => {
+          // same shape as initial values
+          await onRegistation(values)
+          console.log(values);
+          resetForm()
+        }}
+      >
+        {(({ errors, touched }) => <Form>
+          <Field placeholder='Name' className={styles.input} name='name' />{errors.name && touched.name ? (<div>{errors.name}</div>) : null}
+          <Field placeholder='Email' className={styles.input} name='email' />{errors.email && touched.email ? (<div>{errors.email}</div>) : null}
+          <Field placeholder='Password' className={styles.input} name='password' />{errors.password && touched.password ? (<div>{errors.password}</div>) : null}
+          <Button label="Registration" color="orange" type="submit" />
+        </Form>)}
+      </Formik>
+      {/* <form onSubmit={handleOnsubmit}>
         <Input
           type="text"
           onChange={handleNameChange}
@@ -49,8 +80,9 @@ export default function RegistrationView() {
           value={password}
         />
         <Button label="Registration" color="orange" type="submit" />
-      </form>
+      </form> */}
       <CustomNavlink to="/registration" label='Login' color='orange' />
+      <ValidationSchemaExample />
     </div>
   );
 }
