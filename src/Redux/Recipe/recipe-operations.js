84b6a3076c8c5b0db275cdad7df111da423d6949
AnastasiaRiabova <AnastasiaRiabova@users.noreply.github.com>
2021-01-17 16:03:44 +0100

    fix operations

--- src/Redux/Recipe/recipe-operations.js
+++ src/Redux/Recipe/recipe-operations.js
@@ -1,17 +1,18 @@
-import axios from "axios";
-import action from "./recipe-actions";
-axios.defaults.baseURL = "https://api.edamam.com";
+import axios from 'axios';
+import action from './recipe-actions';
 
-const getAllRecipes = (search) => (dispatch) => {
-  console.log(search);
+const getAllRecipes = search => dispatch => {
+  axios.defaults.baseURL = 'https://api.edamam.com';
+  axios.defaults.headers.common.Authorization = '';
+  // console.log(search);
   dispatch(action.getRecipesRequest());
 
   axios
     .get(
-      `/search?q=${search}&app_id=4201534d&app_key=ac730a8b8a048ad137d8d0e0fc6fbb98`
+      `/search?q=${search}&app_id=4201534d&app_key=ac730a8b8a048ad137d8d0e0fc6fbb98`,
     )
-    .then((response) => dispatch(action.getRecipesSuccess(response.data)))
-    .catch((error) => dispatch(action.getRecipesError(error)));
+    .then(response => dispatch(action.getRecipesSuccess(response.data)))
+    .catch(error => dispatch(action.getRecipesError(error)));
 };
 
 export default getAllRecipes;
